fa269fbb3c95a6bbd103c779a2bf834e5b036068
samtech37 <samkondori@gmail.com>
2025-07-10 11:53:57 +0000

    Update configuration and scripts for improved setup; modify max_n_gaussians settings, add jumping and squats data files, and enhance conda installation commands.

--- scripts/conda_setup.sh
+++ scripts/conda_setup.sh
@@ -8,14 +8,16 @@ eval "$(conda shell.bash hook)"
 conda create -n hugs python=3.8 -y
 
 conda activate hugs
+conda install -c nvidia/label/cuda-11.7.0 cuda-toolkit -y
 
 conda install -y pytorch==1.13.1 torchvision==0.14.1 torchaudio==0.13.1 pytorch-cuda=11.7 -c pytorch -c nvidia
 
-pip install fvcore iopath
-pip install --no-index --no-cache-dir pytorch3d -f https://dl.fbaipublicfiles.com/pytorch3d/packaging/wheels/py38_cu117_pyt1131/download.html
 
-pip install submodules/diff-gaussian-rasterization
-pip install submodules/simple-knn
+python -m pip install --no-index --no-cache-dir pytorch3d -f https://dl.fbaipublicfiles.com/pytorch3d/packaging/wheels/py38_cu117_pyt1131/download.html
+python -m pip install fvcore iopath
 
-pip install -r requirements.txt
-pip install git+https://github.com/mattloper/chumpy.git
+python -m pip install submodules/diff-gaussian-rasterization
+python -m pip install submodules/simple-knn
+
+python -m pip install -r requirements.txt
+python -m pip install git+https://github.com/mattloper/chumpy.git
